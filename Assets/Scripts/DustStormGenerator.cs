dd50c34ae08afb444a57cb6d22d5494869f5cdf1
Fumiaki TOKUHISA <tokuhisa.f@gmail.com>
2015-04-11 20:36:42 +0900

    オブジェクトの衝突箇所に砂埃を発生させるDustStormGeneratorクラスを追加

---
+++ Assets/Scripts/DustStormGenerator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DustStormGenerator : MonoBehaviour {
+	
+	private bool dustVisible = false;
+	private GameObject dustPrefab;
+	private GameObject dustObj;
+	// Use this for initialization
+	void Start () {
+		dustPrefab = (GameObject)Resources.Load("ParticleSystems/Prefabs/DustStorm");
+		dustObj = (GameObject)Instantiate(dustPrefab, transform.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveDustStorm();
+		
+	}
+	void OnCollisionEnter(Collision collision) {
+		showDustStorm();
+	}
+	
+	void OnCollisionExit(Collision collisionInfo) {
+		hideDustStorm();
+	}
+	
+	public void showDustStorm() {
+		dustVisible = true;
+		dustObj.GetComponent<ParticleSystem>().Play();
+	}
+	
+	public void hideDustStorm() {
+		dustVisible = false;
+		dustObj.GetComponent<ParticleSystem>().Stop();
+	}
+	
+	public void moveDustStorm() {
+		dustObj.transform.position = transform.position;
+	}
+}
