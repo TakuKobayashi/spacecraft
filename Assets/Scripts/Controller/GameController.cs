dd27b6b9992f53ec203aedf94d434730778d92b1
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-04-12 12:15:28 +0900

    Playerがころころ動くので、GameController直下に作るようにしてみた

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -4,13 +4,21 @@ using System.Collections;
 
 public partial class GameController : SingletonBehaviour<GameController> 
 {
-	[SerializeField] Field field;
-
+	[SerializeField] Prefab dustStormPrefab;
+	GameObject dustStorm;
+	
 	/**
 	 * Application Initialize
 	 */
 	public override void SingleAwake(){
 
+	}
+
+	public void dust(Vector3 position){
+		if(dustStorm == null){
+			dustStorm = Util.InstantiateTo(this.gameObject, dustStormPrefab);
+			dustStorm.transform.position = position;
+		}
 	}
 }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,12 @@ public class Player : MonoBehaviour {
 	[SerializeField] Prefab beamPrefab;
 	[SerializeField] Prefab dustStormPrefab;
 	GameObject beamObject;
-	GameObject dustStorm;
 
 	void Start(){
 		beamObject = Util.InstantiateTo (gameObject, beamPrefab);
 		Beam beam = beamObject.GetComponent<Beam>();
 		beam.OnBeamHit = (RaycastHit hit) => {
-			if(dustStorm == null){
-				dustStorm = Util.InstantiateTo(this.gameObject, dustStormPrefab);
-				dustStorm.transform.position = hit.point;
-			}
+			GameController.Instance.dust (hit.point);
 		};
 	}
 	
