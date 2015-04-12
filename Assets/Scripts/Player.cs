19ee6daa3be4e30b4b38b7afd1f1b1c6ee6dbad2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-04-12 11:40:04 +0900

    土煙出ているはずなのだが…

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,27 +2,24 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-	[SerializeField] Beam beam;
+	[SerializeField] Prefab beamPrefab;
 	[SerializeField] Prefab dustStormPrefab;
+	GameObject beamObject;
 	GameObject dustStorm;
 
 	void Start(){
+		beamObject = Util.InstantiateTo (gameObject, beamPrefab);
+		Beam beam = beamObject.GetComponent<Beam>();
 		beam.OnBeamHit = (RaycastHit hit) => {
 			if(dustStorm == null){
 				dustStorm = Util.InstantiateTo(this.gameObject, dustStormPrefab);
 				dustStorm.transform.position = hit.point;
 			}
 		};
-		beam.OnBeamFinish = () => {
-			if(dustStorm != null){
-				dustStorm = null;
-				Destroy(dustStorm);
-			}
-		};
 	}
 	
 	void Update(){
-		beam.transform.position = this.transform.position;
-		beam.transform.position = this.transform.position;
+		beamObject.transform.position = this.transform.position;
+		beamObject.transform.position = this.transform.position;
 	}
 }
